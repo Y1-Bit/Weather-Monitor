56dabcdbdf3e27daf6536e61a29cffd69fc992cd
Y1-Bit <y0749701@gmail.com>
2024-10-14 18:58:15 +0300

    Remove precipitation amount from func

--- utils.py
+++ utils.py
@@ -10,15 +10,12 @@ def convert_pressure_to_mm_hg(surface_pressure: float) -> float:
 
 def get_precipitation_info(
     current_snowfall: float, current_rain: float, current_showers: float
-) -> tuple:
+) -> str:
     if current_snowfall > 0:
         precipitation_type = "снег"
-        precipitation_amount = current_snowfall
     elif current_rain > 0 or current_showers > 0:
         precipitation_type = "дождь"
-        precipitation_amount = current_rain + current_showers
     else:
         precipitation_type = "нет осадков"
-        precipitation_amount = 0.0
 
-    return precipitation_type, precipitation_amount
+    return precipitation_type
