663366db15aa59b50c7f2b538edb3e44e27a9ab5
Y1-Bit <y0749701@gmail.com>
2024-10-17 18:03:30 +0300

    WeatherDataModel for WeatherAPI

--- weather_api.py
+++ weather_api.py
@@ -4,6 +4,7 @@ from openmeteo_sdk.WeatherApiResponse import WeatherApiResponse
 from retry_requests import retry
 
 from config import LATITUDE, LONGITUDE
+from schemas import WeatherDataModel
 from utils import (
     convert_pressure_to_mm_hg,
     convert_wind_direction,
@@ -11,7 +12,7 @@ from utils import (
 )
 
 
-def fetch_weather_data(latitude: float, longitude: float):
+def fetch_weather_data(latitude: float, longitude: float) -> WeatherApiResponse:
     cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
     openmeteo = openmeteo_requests.Client(session=retry_session)
@@ -37,23 +38,23 @@ def fetch_weather_data(latitude: float, longitude: float):
     return responses[0]
 
 
-def get_current_weather(current_data: WeatherApiResponse):
+def get_current_weather(current_data: WeatherApiResponse) -> WeatherDataModel:
     current = current_data.Current()
     if current is None:
         raise ValueError("Weather data not fetched. Call 'fetch_weather_data()' first.")
 
-    return {
-        "temperature": current.Variables(0).Value(),
-        "wind_speed": current.Variables(6).Value(),
-        "wind_direction": convert_wind_direction(current.Variables(7).Value()),
-        "pressure": convert_pressure_to_mm_hg(current.Variables(5).Value()),
-        "precipitation": current.Variables(1).Value(),
-        "precipitation_type": get_precipitation_info(
+    return WeatherDataModel(
+        temperature=current.Variables(0).Value(),
+        wind_speed=current.Variables(6).Value(),
+        wind_direction=convert_wind_direction(current.Variables(7).Value()),
+        pressure=convert_pressure_to_mm_hg(current.Variables(5).Value()),
+        precipitation_amount=current.Variables(1).Value(),
+        precipitation_type=get_precipitation_info(
             current.Variables(4).Value(),
             current.Variables(2).Value(),
             current.Variables(3).Value(),
         ),
-    }
+    )
 
 
 if __name__ == "__main__":
@@ -61,9 +62,9 @@ if __name__ == "__main__":
     current_weather = get_current_weather(current_data)
 
     print("Текущая погода:")
-    print(f"Температура: {current_weather['temperature']}°C")
-    print(f"Скорость ветра: {current_weather['wind_speed']} м/с")
-    print(f"Направление ветра: {current_weather['wind_direction']}")
-    print(f"Давление: {current_weather['pressure']} мм рт. ст.")
-    print(f"Тип осадков: {current_weather['precipitation_type']}")
-    print(f"Количество осадков: {current_weather['precipitation']} мм")
+    print(f"Температура: {current_weather.temperature}°C")
+    print(f"Скорость ветра: {current_weather.wind_speed} м/с")
+    print(f"Направление ветра: {current_weather.wind_direction}")
+    print(f"Давление: {current_weather.pressure} мм рт. ст.")
+    print(f"Тип осадков: {current_weather.precipitation_type}")
+    print(f"Количество осадков: {current_weather.precipitation_amount} мм")
