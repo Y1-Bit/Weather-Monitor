d2d58f474ef854812b1729221a1b603c5d6c99e5
Y1-Bit <y0749701@gmail.com>
2024-10-17 17:58:43 +0300

    WeatherDataModel

---
+++ schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+class WeatherDataModel(BaseModel):
+    temperature: float
+    wind_direction: str
+    wind_speed: float
+    pressure: float
+    precipitation_type: str
+    precipitation_amount: float
