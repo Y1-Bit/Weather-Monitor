3dc49ae82612e7f1a945eca0452bdc6c03b3e68f
Y1-Bit <y0749701@gmail.com>
2024-10-18 20:27:18 +0300

    Get latest weather data from DB

--- app/database/repo/weater_data.py
+++ app/database/repo/weater_data.py
@@ -1,6 +1,6 @@
 from app.database.models.weater_data import WeatherData
 from app.database.repo.base import BaseRepo
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 
 from app.schemas.weather_schema import WeatherDataModel, WeatherDataModelList
 
@@ -18,3 +18,22 @@ class WeatherDataRepo(BaseRepo):
 
         await self.session.execute(insert_stmt)
         await self.session.commit()
+
+    async def get_latest_weather_data(self, limit: int = 10) -> WeatherDataModelList:
+        query = select(WeatherData).order_by(WeatherData.created_at.desc()).limit(limit)
+        result = await self.session.execute(query)
+        weather_data_objects = result.scalars().all()
+
+        return WeatherDataModelList(
+            weather_data=[
+                WeatherDataModel(
+                    temperature=data.temperature,
+                    wind_direction=data.wind_direction,
+                    wind_speed=data.wind_speed,
+                    pressure=data.pressure,
+                    precipitation_type=data.precipitation_type,
+                    precipitation_amount=data.precipitation_amount,
+                )
+                for data in weather_data_objects
+            ]
+        )
