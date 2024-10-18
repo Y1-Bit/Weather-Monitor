fc08eb54c06c8559aec33729be8f739501fbc667
Y1-Bit <y0749701@gmail.com>
2024-10-18 21:16:02 +0300

    Add scheduler with interval task

--- app/main.py
+++ app/main.py
@@ -1,19 +1,41 @@
 import asyncio
+import logging
 
-from app.config import LATITUDE, LONGITUDE, get_db_url
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from app.config import INTERVAL_MINUTES, LATITUDE, LONGITUDE, get_db_url
 from app.database.setup import create_engine, create_session_pool
-from app.services.data_service import DataExportService
 from app.services.weather_service import WeatherService
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def fetch_weather(weather_service: WeatherService):
+    try:
+        await weather_service.fetch_and_store_weather(LATITUDE, LONGITUDE)
+        logging.info("Данные о погоде обновлены.")
+    except Exception as e:
+        logging.error(f"Ошибка при обновлении данных о погоде: {e}")
 
 
 async def main():
-    engine = create_engine(get_db_url(), echo=True)
+    engine = create_engine(get_db_url(), echo=False)
     session_pool = create_session_pool(engine)
     weather_service = WeatherService(session_pool)
-    data_export_service = DataExportService(weather_service)
 
-    await data_export_service.export_latest_weather_data_to_xlsx("data.xlsx")
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(
+        fetch_weather, "interval", minutes=INTERVAL_MINUTES, args=[weather_service]
+    )
+    scheduler.start()
+
+    logging.info("Шедулер запущен. Ожидание задач...")
+
+    while True:
+        await asyncio.sleep(1)
+
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
